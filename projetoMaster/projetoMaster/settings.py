ff2005429ad9ddc276da0448c9c5de20a624dc0f
user <user@r-one.r-one-server>
2016-08-16 21:15:25 -0300

    mudança de banco de dados para mysql

--- projetoMaster/projetoMaster/settings.py
+++ projetoMaster/projetoMaster/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'projetoMaster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projeto101',
+	'USER': 'root',
+	'PASSWORD': 'capcom-r1#',
+#	'HOST': '127.0.0.1',
+	'PORT':'8080',
     }
 }
 
